14bee17169aee05f9f003ccbe4e5edacc38329f9
carlostrullo <carlos.2396@hotmail.com>
2016-11-08 07:20:03 -0500

    Update test_project.sh

--- test_project.sh
+++ test_project.sh
@@ -3,6 +3,3 @@ set -e
 
 . ~/. flask_env/bin/activate
 
-PYTHONPATH=. py.test --junitxml=python_tests.xml
-PYTHONPATH=. py.test --cov-report xml --cov=../
-PYTHONPATH=. py.test --cov-report html --cov=../
