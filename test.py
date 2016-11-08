cf2de48d0c0a241f4545312002a4a55597f01e27
carlostrullo <carlos.2396@hotmail.com>
2016-11-08 11:50:05 -0500

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def test_files_post():
   content= {"filename": "escribe2","content": "prueba archivo"}
   data_json=json.dumps(content)
   headers = {'content-type': 'application/json'}
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('POST','/v1.0/files',data_json, headers)
   response=conn.getresponse()
   assert response.status==201
@@ -27,28 +27,37 @@ def test_files_post():
 
 
 def test_files_get():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('GET','/v1.0/files')
   response=conn.getresponse()
   assert response.status==200
   conn.close()
 
+
+def test_files_delete():
+  conn = httplib.HTTPConnection("localhost:8088")
+  conn.request('DELETE','/v1.0/files')
+  response=conn.getresponse()
+  assert response.status==200
+  conn.close()
+    
+
 def test_files_put():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('PUT','/v1.0/files')
   response=conn.getresponse()
   assert response.status==404
   conn.close()
 
 def test_files_recently_created():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('GET','/v1.0/files/recently_created')
   response=conn.getresponse()
   assert response.status==200
   conn.close()
 
 def test_files_recently_created_post():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('POST','/v1.0/files/recently_created')
   response=conn.getresponse()
   assert response.status==404
@@ -56,7 +65,7 @@ def test_files_recently_created_post():
 
 
 def test__files_recently_created_put():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('PUT','/v1.0/files/recently_created')
   response=conn.getresponse()
   assert response.status==404
@@ -65,7 +74,7 @@ def test__files_recently_created_put():
 
 
 def test__files_recently_created_delete():
-  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn = httplib.HTTPConnection("localhost:8088")
   conn.request('DELETE','/v1.0/files/recently_created')
   response=conn.getresponse()
   assert response.status==404
