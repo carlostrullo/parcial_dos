53f67e06a97fa2a54c44231c54d81d26f163e08c
carlostrullo <carlos.2396@hotmail.com>
2016-11-07 19:40:08 -0500

    solución en pytest

---
+++ test.py
@@ -0,0 +1,72 @@
+from flask import Flask, abort, request
+import os
+import json
+import pytest
+import parcial
+import httplib, urllib
+
+
+@pytest.fixture
+def app():
+    return parcial.app
+
+@pytest.fixture
+def test_client(app):
+    return app.test_client()
+
+
+def test_files_post():
+  content= {"filename": "escribe2","content": "prueba archivo"}
+  data_json=json.dumps(content)
+  headers = {'content-type': 'application/json'}
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('POST','/v1.0/files',data_json, headers)
+  response=conn.getresponse()
+  assert response.status==201
+  conn.close()
+
+
+def test_files_get():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('GET','/v1.0/files')
+  response=conn.getresponse()
+  assert response.status==200
+  conn.close()
+
+def test_files_put():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('PUT','/v1.0/files')
+  response=conn.getresponse()
+  assert response.status==404
+  conn.close()
+
+def test_files_recently_created():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('GET','/v1.0/files/recently_created')
+  response=conn.getresponse()
+  assert response.status==200
+  conn.close()
+
+def test_files_recently_created_post():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('POST','/v1.0/files/recently_created')
+  response=conn.getresponse()
+  assert response.status==404
+  conn.close()
+
+
+def test__files_recently_created_put():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('PUT','/v1.0/files/recently_created')
+  response=conn.getresponse()
+  assert response.status==404
+  conn.close()
+
+
+
+def test__files_recently_created_delete():
+  conn = httplib.HTTPConnection("192.168.1.31:8088")
+  conn.request('DELETE','/v1.0/files/recently_created')
+  response=conn.getresponse()
+  assert response.status==404
+  conn.close()
